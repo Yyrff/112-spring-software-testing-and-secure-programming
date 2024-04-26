6f7a8245215bc36fa9937c87f5d586736e60707c
CHIH MING CHEN <129422182+Yyrff@users.noreply.github.com>
2024-04-26 14:00:09 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,27 +1,23 @@
 const puppeteer = require('puppeteer');
-
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-
-    await page.goto('https://pptr.dev/');
-    await page.waitForSelector('.DocSearch-Button'); 
-    await page.click('.DocSearch-Button'); 
-   
-    const searchInputSelector = '#docsearch-input';
-    await page.waitForSelector(searchInputSelector);
-    await page.type(searchInputSelector, 'chipi chipi chapa chapa', {delay: 50});
-
-    const searchResultSelector = '#docsearch-item-5 > a > div > div.DocSearch-Hit-content-wrapper > span.DocSearch-Hit-path';
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector, {delay: 3000});
-    
-    const titleSelector = 'h1';
-    await page.waitForSelector(titleSelector, {delay: 1000});
-    await page.click(titleSelector);
-
-    const title = await page.$eval(titleSelector, element => element.textContent);
-    console.log(title);
-    
+    await page.goto('https://pptr.dev/webdriver-bidi/#measuring-progress');
+    await page.waitForSelector('.DocSearch-Button-Placeholder'); 
+    await page.click('.DocSearch-Button-Placeholder'); 
+    await page.waitForSelector('.DocSearch-Input'); 
+    await page.type('.DocSearch-Input', 'chipi chipi chapa chapa'); //input
+    await page.waitForSelector('.DocSearch-Hits')
+    await page.waitForFunction(() => {
+        const hit = Array.from(document.querySelectorAll('.DocSearch-Hit-title')).find(e => e.textContent.includes('To gauge the capabilities of'));
+        return hit && hit.closest('.DocSearch-Hit-Container');
+    });
+    await page.evaluate(() => {
+        const hit = Array.from(document.querySelectorAll('.DocSearch-Hit-title')).find(e => e.textContent.includes('To gauge the capabilities of'));
+        hit && hit.closest('.DocSearch-Hit-Container').click();
+    });
+    await page.waitForSelector('h1', { visible: true });
+    const headerText = await page.$eval('h1', element => element.textContent);
+    console.log(headerText);  
     await browser.close();
 })(); 
