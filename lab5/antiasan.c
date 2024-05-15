087ff1cbdb69bcab7261a24da29978eb495c5bd0
CHIH MING CHEN <129422182+Yyrff@users.noreply.github.com>
2024-05-15 13:59:53 +0800

    Update antiasan.c

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,11 +1,8 @@
-#include <stddef.h>
-#include <stdint.h>   
-
 void antiasan(unsigned long addr) {
-    unsigned long shadow_addr = (addr >> 3) + 0x7fff8000;
-    *(char *)shadow_addr = 0;
+    unsigned long shadowAddress;
+    char *shadowByte;
 
-// TODO:
-void antiasan(unsigned long addr)
-
+    shadowAddress = (addr >> 3) + 0x7fff8000;
+    shadowByte = (char *)shadowAddress;
+    *shadowByte = 0;
 }
